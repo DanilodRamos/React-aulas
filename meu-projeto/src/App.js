95fc9a1549bcee641c5a5cd9dc9f66bcf3335cc1
danilo <danilodramos@yahoo.com.br>
2024-01-22 10:45:32 -0300

    aula 1
    instalando e fazendo modificações

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Olá react</h1>
+     <p>estou fazendo um projeto react</p>
+    </div>
+  );
+}
+
+export default App;
